c215c140a15b6e907d37657c21729221e596fa1f
Shivam Mehta <shivam.mehta007@gmail.com>
2022-11-07 12:37:52 +0000

    Adding cmd to transcriber

--- whisperweranalysis/transcriber.py
+++ whisperweranalysis/transcriber.py
@@ -1,4 +1,4 @@
-import logging
+import argparse
 from pathlib import Path
 
 import whisper
@@ -7,8 +7,6 @@ from tqdm import tqdm
 WORKDIR = Path(__file__).parent
 DATADIR = WORKDIR / 'LJ_Valid_data'
 TRANSCRIPTIONDIR = WORKDIR / 'LJ_Valid_transcription'
-
-logger = logging.getLogger(__name__)
 
 class Whisper:
     """
@@ -21,41 +19,48 @@ class Whisper:
     """
     def __init__(self, model="large"):
         self.model = whisper.load_model(model)
-        logger.info("Whisper model loaded")
+        print("[+] Whisper model loaded")
 
     def transcribe(self, audio_path, lang="en"):
         result = self.model.transcribe(audio_path, language=lang)
         return result["text"]
     
-    def transcribe_folder(self, folder_path, transcription_datadir=TRANSCRIPTIONDIR, exception=None):
+    def transcribe_folder(self, input_dir, output_dir, exception=None):
         if exception:
             if isinstance(exception, list):
                 exception = set(exception)
             if isinstance(exception, str):
                 exception = set([exception])
 
-        logger.info(f"Running whisper over: {folder_path}")
-        folder_path = Path(folder_path)
-        filelist = list(folder_path.rglob('*.*'))
+        print(f"Running whisper over: {input_dir}")
+        input_dir = Path(input_dir)
+        filelist = list(input_dir.rglob('*.*'))
         for filepath in tqdm(filelist, leave=False):
             if exception and filepath.parent.parent.name in exception:    # change here the location to exclude
-                print(f"\r Skipping: {exception}")
+                print(f"\r[!] Skipping: {exception}")
                 continue
-            transcription_path = Path(str(filepath).replace(str(DATADIR), str(transcription_datadir)).replace(".wav", ".txt"))
+            transcription_path = Path(str(filepath).replace(str(DATADIR), str(output_dir)).replace(".wav", ".txt"))
             transcription_path.parent.mkdir(parents=True, exist_ok=True)
             
             if transcription_path.exists():
-                print(f"\r Skipping: {transcription_path}")
+                print(f"\r[!] Skipping: {transcription_path} it already exists!")
                 continue
             
             text = self.transcribe(str(filepath))
 
             with open(transcription_path, 'w') as f:
                 f.write(text)
-        logger.info(f"Transcriptions saved to {transcription_datadir}")
-
+        print(f"[+] Transcriptions saved to {output_dir}")
 
 
 if __name__ == '__main__':
-    whisper = Whisper("medium")
-    whisper.transcribe_folder(DATADIR, exception=["NS", "OverFlow"])
+    model_choices = ["large", "medium", "small"]
+    parser = argparse.ArgumentParser(description='Run whisper on a folder')
+    parser.add_argument("-m", "--model", default=model_choices[1], help="Model to use", choices=model_choices)
+    parser.add_argument("-i", "--input", help="Input folder", required=True)
+    parser.add_argument("-o", "--output", help="Output folder", required=True)
+    parser.add_argument('-e','--exceptions', nargs='+', help='Subfolders to exclude', default=None)
+
+    args = parser.parse_args()
+    whisper = Whisper(model=args.model)
+    whisper.transcribe_folder(args.input, args.output, exception=args.exceptions)
