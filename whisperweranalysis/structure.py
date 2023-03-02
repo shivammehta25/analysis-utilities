00f47f67575a898e9d9b4f91fac594199d319b83
Shivam Mehta <shivam.mehta25@gmail.com>
2023-03-02 15:45:55 +0000

    FP and VITS wer

--- whisperweranalysis/structure.py
+++ whisperweranalysis/structure.py
@@ -16,33 +16,37 @@ class MultipleSets:
             hvd_set = HvdSet(set_no, hvd_sentences)
             hvd_set.compute_wer(transcription_loc)
             self.harvard_sets.append(hvd_set)
-        
+
     def __getitem__(self, index):
         return self.harvard_sets[index - 1]
-    
+
     def __iter__(self):
         return iter(self.harvard_sets)
-    
+
     def __len__(self):
         return len(self.harvard_sets)
-    
-    
+
     def get_all_models_wer(self):
         assert len(self.harvard_sets) > 0, "No sets have been computed yet"
-        models = defaultdict(lambda : defaultdict(float))
+        models = defaultdict(lambda: defaultdict(float))
         for i, set_ in enumerate(self):
             for model in set_.wer:
-                models[model][i+1] = set_.wer[model]
+                models[model][i + 1] = set_.wer[model]
         lens = [len(models[model]) for model in models]
-        
+
         assert min(lens) == max(lens), "Not all models have the same number of sets"
         # Cast to dict from default dict and sorted by value
-        return dict({key : sorted(dict(value).items(), key=lambda x: x[1]) for key, value in models.items()})
-        
+        return dict(
+            {
+                key: sorted(dict(value).items(), key=lambda x: x[1])
+                for key, value in models.items()
+            }
+        )
+
     def get_top_n(self, n=10):
         models = self.get_all_models_wer()
         iterative_set = defaultdict(set)
-        
+
         def get_set_intersection(defaultdict_):
             return set.intersection(*defaultdict_.values())
 
@@ -56,7 +60,7 @@ class MultipleSets:
             # if the intersection is to the number required, return
             if len(final_set) >= n:
                 break
-    
+
         indices = sorted(list(final_set))
         return [self[i] for i in indices]
 
@@ -68,12 +72,12 @@ class HvdSet:
         self.end = (set_no) * 10
         self.transcriptions = defaultdict(list)
         self.wer = defaultdict(float)
-        
+
         if isinstance(ground_truth, str):
             with open(ground_truth) as f:
                 ground_truth = f.readlines()
-        self.ground_truth = ground_truth[self.begin - 1: self.end]
-    
+        self.ground_truth = ground_truth[self.begin - 1 : self.end]
+
     def __repr__(self) -> str:
         return f"HvdSet({self.set_no}, {self.begin}, {self.end})"
 
@@ -82,7 +86,8 @@ class HvdSet:
         for folder in tqdm(folders, leave=False):
             for i in range(self.begin, self.end + 1):
                 with open(f"{folder / folder.name}_{i}.txt") as f:
-                    self.transcriptions[folder.name].append(f.readline().strip())
+                    text = f.readline().strip()
+                    self.transcriptions[folder.name].append(text if text != "" else ".")
         self.transcriptions = dict(self.transcriptions)
 
     def compute_wer(self, dir):
@@ -104,58 +109,60 @@ class HvdSet:
         for model, text in self.transcriptions.items():
             self.wer[model] = compute(self.ground_truth, text)
         self.wer = dict(self.wer)
-        
-        
-        
+
+
 class LJSpeech:
-    
-    def __init__(self, model_name, iterations='') -> None:
+    def __init__(self, model_name, iterations="") -> None:
         self.model_name = model_name
         self.iterations = iterations
         self.transcriptions = []
-        
+
     def __repr__(self) -> str:
         return f"LJSpeech({self.model_name}, {self.iterations}, len={len(self)})"
 
     def __len__(self):
         return len(self.transcriptions)
-    
+
     def load_transcriptions(self, dir_path):
         if not isinstance(dir_path, Path):
             dir_path = Path(dir_path)
-        dirs = sorted(list(dir_path.glob('*.txt')), key= lambda x: int(x.stem))
+        dirs = sorted(list(dir_path.glob("*.txt")), key=lambda x: int(x.stem))
         for file in dirs:
-            with open(file, 'r') as f:
+            with open(file) as f:
                 line = f.readline().strip()
-                self.transcriptions.append(line if line else '.')
-                
+                self.transcriptions.append(line if line else ".")
+
     def compute_wer(self, ground_truth):
         self.wer = compute(ground_truth, self.transcriptions)
         return self.wer
-                
+
     @classmethod
     def load_from_dir(cls, dir_path):
         if not isinstance(dir_path, Path):
             dir_path = Path(dir_path)
-        
+
         iter_name = dir_path.name
         model_name = dir_path.parent.name
-        lj_class =  cls(model_name, iter_name)
+        lj_class = cls(model_name, iter_name)
         lj_class.load_transcriptions(dir_path)
         return lj_class
-            
+
 
 if __name__ == "__main__":
     # from whisperweranalysis.groundtruth import hvd_sentences
     # hvd = HvdSet(1, hvd_sentences)
     # hvd.compute_wer('whisperweranalysis/transcription')
-    
-    
+
     # from whisperweranalysis.structure import MultipleSets
     # hvd_sets = MultipleSets('/home/shivam/Projects/analysis-scripts/WhisperWERAnalysis/whisperweranalysis/transcription')
     # hvd_sets.get_top_n()
-    
-    
-    l = LJSpeech.load_from_dir('whisper-analysis-plots/whisperweranalysis/LJ_Valid_transcription/VOC/1000')
-    from whisperweranalysis.groundtruth import lj_valid
-    l.compute_wer(lj_valid)
+
+    # l = LJSpeech.load_from_dir('whisper-analysis-plots/whisperweranalysis/LJ_Valid_transcription/VOC/1000')
+    # from whisperweranalysis.groundtruth import lj_valid
+    # l.compute_wer(lj_valid)
+
+    from whisperweranalysis.structure import MultipleSets
+
+    hvd_sets = MultipleSets(
+        "/home/smehta/Projects/whisper-analysis-plots/whisperweranalysis/transcription"
+    )
