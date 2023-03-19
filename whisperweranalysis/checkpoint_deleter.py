ba03a398ccca609db46c9d7adf32e0bf37d86307
Shivam Mehta <shivam.mehta25@gmail.com>
2023-03-19 09:44:30 +0000

    Adding recursively subfolder deleting

--- whisperweranalysis/checkpoint_deleter.py
+++ whisperweranalysis/checkpoint_deleter.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import click
@@ -5,23 +6,29 @@ import click
 from whisperweranalysis.checkpoint_mover import get_list_iter
 
 
+def remove_checkpoint_in_folder(checkpoint_dir, ext, checkpoints_to_keep):
+    for checkpoint in Path(checkpoint_dir).glob(f"checkpoint_*{ext}"):
+        if int(checkpoint.stem.split("_")[-1]) not in checkpoints_to_keep:
+            click.secho("[-] Deleting: " + str(checkpoint), fg="red")
+            checkpoint.unlink()
+        else:
+            click.secho("[+] Keeping: " + str(checkpoint), fg="green")
+
+
 @click.command()
 @click.option(
-    "--checkpoint_dir",
+    "--dir_path",
     "-i",
     type=click.Path(exists=True),
     required=True,
     help="Path to checkpoint directory where checkpoints will be filtered",
 )
 @click.option("--ext", "-e", default=".ckpt", help="Extension of checkpoints to filter")
-def main(checkpoint_dir, ext):
+def main(dir_path, ext):
     checkpoints_to_keep = get_list_iter()
-    for checkpoint in Path(checkpoint_dir).glob(f"*{ext}"):
-        if int(checkpoint.stem.split("_")[-1]) not in checkpoints_to_keep:
-            click.secho("[-] Deleting: " + str(checkpoint), fg="red")
-            checkpoint.unlink()
-        else:
-            click.secho("[+] Keeping: " + str(checkpoint), fg="green")
+    for path, _, _ in os.walk(dir_path):
+        click.secho("[+] Filtering: " + path, fg="blue")
+        remove_checkpoint_in_folder(path, ext, checkpoints_to_keep)
 
 
 if __name__ == "__main__":
