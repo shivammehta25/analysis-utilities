beeca6f146c1625d2a73b09482793c929bbf15b1
Shivam Mehta <shivam.mehta25@gmail.com>
2023-08-01 18:50:49 +0000

    Adding further options to remove checkpoints

--- whisperweranalysis/checkpoint_deleter.py
+++ whisperweranalysis/checkpoint_deleter.py
@@ -6,9 +6,13 @@ import click
 from whisperweranalysis.checkpoint_mover import get_list_iter
 
 
-def remove_checkpoint_in_folder(checkpoint_dir, ext, checkpoints_to_keep):
-    for checkpoint in Path(checkpoint_dir).glob(f"checkpoint_*{ext}"):
-        if int(checkpoint.stem.split("_")[-1]) not in checkpoints_to_keep:
+def remove_checkpoint_in_folder(
+    checkpoint_dir, ext, checkpoints_to_keep, filename, splitter
+):
+    for checkpoint in Path(checkpoint_dir).glob(
+        f"{filename.replace('<ast>', '*')}{ext}"
+    ):
+        if int(checkpoint.stem.split(f"{splitter}")[-1]) not in checkpoints_to_keep:
             click.secho("[-] Deleting: " + str(checkpoint), fg="red")
             checkpoint.unlink()
         else:
@@ -24,11 +28,18 @@ def remove_checkpoint_in_folder(checkpoint_dir, ext, checkpoints_to_keep):
     help="Path to checkpoint directory where checkpoints will be filtered",
 )
 @click.option("--ext", "-e", default=".ckpt", help="Extension of checkpoints to filter")
-def main(dir_path, ext):
+@click.option(
+    "--filename",
+    "-f",
+    default="checkpoint_<ast>",
+    help="Filename of checkpoints to filter",
+)
+@click.option("--splitter", "-s", default="_", help="Split integer on this character")
+def main(dir_path, ext, filename, splitter):
     checkpoints_to_keep = get_list_iter()
     for path, _, _ in os.walk(dir_path):
         click.secho("[+] Filtering: " + path, fg="blue")
-        remove_checkpoint_in_folder(path, ext, checkpoints_to_keep)
+        remove_checkpoint_in_folder(path, ext, checkpoints_to_keep, filename, splitter)
 
 
 if __name__ == "__main__":
